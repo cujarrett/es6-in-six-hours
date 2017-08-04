ca7fcdbf0df775a80b1139a1b24203838620aa43
matt-jarrett <cujarrett@gmail.com>
2017-08-04 18:30:43 -0500

    Adds clearer logging to lesson.

--- src/01-a-let.js
+++ src/01-a-let.js
@@ -7,20 +7,20 @@ function varTest() {
   var x = 1
   if (true) {
     var x = 2 // same variable!
-    console.log(x) // 2
+    console.log("varTest: x = " + x) // 2
   }
-  console.log(x) // 2
+  console.log("varTest: x = " + x) // 2
 }
 
 function letTest() {
   let x = 1
   if (true) {
     let x = 2 // different variable
-    console.log(x) // 2
+    console.log("letTest: x = " + x) // 2
   }
-  console.log(x) // 1
+  console.log("letTest: x = " + x) // 1
 }
 
 // Uncomment these one at a time to demo
 varTest()
-//letTest()
+letTest()
