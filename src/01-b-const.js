95c6ce455b0be3ed71c3c61135d48b21ea8779cb
matt-jarrett <cujarrett@gmail.com>
2017-08-04 18:31:36 -0500

    Cleans up lesson to show block scope and throws error at the end to allow it to error last after all other code runs.

--- src/01-b-const.js
+++ src/01-b-const.js
@@ -3,10 +3,13 @@
 
 // More reading: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
 
+const color = "yellow"
+
 if (true) {
   const color = "blue"
-  console.log("color: " + color) // bar
-  color = "red" // TypeError - Can't reasign const
+  console.log("color: " + color) // blue
 }
 
-console.log("color: " + color) // undefined
+console.log("color: " + color) // yellow
+
+color = "red" // throws TypeError - Can't reasign a const
