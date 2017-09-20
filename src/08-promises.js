e2b7e4ce8ff9c2d677bfd7ac565231d6502efd05
Matt Jarrett <cujarrett@gmail.com>
2017-09-19 20:20:08 -0500

    Adds more examples

--- src/08-promises.js
+++ src/08-promises.js
@@ -4,19 +4,58 @@ const http = require("http")
 
 // More reading: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
 
-// Example 1
+// Example 1 - Shows why Promises are needed
+const printColor = (color) => {
+  console.log(color)
+}
+
+const slowFunction = () => {
+  setTimeout(() => {
+    return "Blue"
+  }, 3000)
+}
+
+const color = slowFunction()
+printColor(color) // undefined
+
+// Example 2 - Shows simple Promise
+const printAColor = (color) => {
+  console.log(color)
+}
+
 let myFirstPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("Success!")
   }, 3000)
 })
 
-myFirstPromise.then((successMessage) => {
-  console.log(`Yay! ${successMessage}`)
-})
+myFirstPromise
+  .then((successMessage) => {
+    console.log(`Yay! ${successMessage}`)
+    return "Purple"
+  })
+  .then(printAColor)
 // Yay! Success!
+// Purple
 
-// Example 2
+// Example 3 - Shows Promise branching
+let mySecondPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Success!")
+  }, 3000)
+})
+
+mySecondPromise
+  .then((successMessage) => {
+    console.log(`Yay! ${successMessage}`)
+  })
+
+mySecondPromise
+  .then((successMessage) => console.log(successMessage.toUpperCase()))
+// Yay! Success!
+// SUCCESS
+
+// Example 4 - Web service real example
 const makeStarWarsApiCall = (resolve, reject) => {
   return new Promise((resolve, reject) => {
     http.get("http://swapi.co/api/films/")
